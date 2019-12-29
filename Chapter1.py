24cf290d0314f21b23aa72b73c89b71428df1f1a
SuperTrampAI <lxh800109@gmail.com>
2019-12-29 16:56:14 +0800

    python learn

---
+++ Chapter1.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+# __author__ = 'lxh800109@gmail.com'
+# __fileName__='baseDemo Chapter'
+# __create_data__='2019/12/29 16:38'
+# python源码文件以UTF-8编码方式处理，如果要更改编码方式，则文件的的第一行增加一个coding
+#
+the_world_is_flat = True
+if the_world_is_flat:
+    print("true")
+
