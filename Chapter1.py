b915f0b35feeabfbbc885c1672c5d111ca7f61a2
SuperTrampAI <lxh800109@gmail.com>
2019-12-29 17:05:30 +0800

    chapter2 commit github test

--- Chapter1.py
+++ Chapter1.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 # __author__ = 'lxh800109@gmail.com'
-# __fileName__='baseDemo Chapter'
+# __fileName__='baseDemo Chapter1'
 # __create_data__='2019/12/29 16:38'
 # python源码文件以UTF-8编码方式处理，如果要更改编码方式，则文件的的第一行增加一个coding
 #
---
+++ Chapter2.py
@@ -0,0 +1,4 @@
+# __author__ = 'lxh800109@gmail.com'
+# __fileName__='Chapters Chapter2'
+# __create_data__='2019/12/29 17:04'
+
