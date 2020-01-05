550978d85d378fd9b722679e56ff2492a44783a2
lixianghong <lxh800109@gmail.com>
2020-01-05 18:20:57 +0800

    chapter3 commit github test 基本语法学习

--- Chapter2.py
+++ Chapter2.py
@@ -2,3 +2,49 @@
 # __fileName__='Chapters Chapter2'
 # __create_data__='2019/12/29 17:04'
 
+# 加减乘除  混合类型运算会将结果的类型转换成最高的那个。比如有int和double，运算的结果会是double
+print(2+2)
+print(50-4*5)
+print((50 - 5*6) / 4)
+print(8 / 5)
+# // 忽略小数部分
+print(9//4)
+# 取得余数
+print(9%4)
+# 乘方
+print(2**3)
+
+# 变量
+num=1
+num1=2
+print(num+num1)
+
+# 字符串  在前面加一个r，可以使得\不在转义 字符串不能修改
+text="ABCDEFGHIJKLMNOPKRSTUVWXYZ"
+print(r'C:\some\name')
+
+# 该位置的\可以去掉换行
+print("""\
+    test
+    testtest
+    test
+""")
+
+# 使用* 可以对字符串进行重复
+print(3*'un'+"ium")
+
+# 两个相邻的会自动连接在一起, 只能是字面量，不能是变量，不能是表达式，连接的不是两个字面量时，使用+号连接
+prefix="1"
+print("test" "test2")
+
+print(prefix+"test")
+
+# 根据下标去字符串是的值
+# 字符串的下标从0开始（0和-0是一样的），也可以从负数开始，起点为-1
+print(text[-1])
+# 字符串的切片：从字符串中获得子字符串
+# 下标也是从0开始
+print(text[0:4])# 从哪个下标开始，第二个参数为截止的下标
+print(len(text))# 返回字符串长度
+
+
---
+++ Chapter3-List.py
@@ -0,0 +1,36 @@
+# __author__ = 'lxh800109@gmail.com'
+# __fileName__='Chapters Chapter3-List'
+# __create_data__='2020/1/5 17:49'
+
+# 一组列表可以包含不同类型的元素，但是通常使用时哥哥元素类型相同 ,列表的值可以通过下标改变
+squares=[1, 2, 3, 4, 5, 6, 7]
+# 索引
+squares[0]
+# 切片 所有的切片都返回一个新列表
+squares[0:]
+# 如下会返回列表的一个的新的浅拷贝
+squares[:]
+# 使用+做拼接操作
+squares +[8,9,0]
+# 使用append()做拼接操作
+squares.append(10)
+print(squares)
+#把第三个到六个替换 ；从下标2开始的的三个，包含下标为2的
+squares[2:5]=[12,13,14]
+print(squares)
+#清空下标2开始的三个
+squares[2:5]=[]
+# 清空整个list
+squares[:]=[]
+print(len(squares))
+
+#多层集合
+letters=[[1,2,3,4],['a','b','c','d']]
+print(letters[0])
+print(letters[1])
+
+a,b=0,1
+while a<10:# 任何非零整数都为真，零为假。
+    print(a) # print()方法的参数end=',' 可以用来取消后面的换行，或者可以用其他字符来分割
+    a,b=b,a+b
+
